a0f11ca42c4bc2d4d126f8296758466429c3136b
LyThiHoangYen <lythihoangyen20052003@gmail.com>
2024-12-14 16:44:33 +0700

    committhem

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 //require_once 'app/config.php'; -->
 <?php
 // Kết nối đến cơ sở dữ liệu
-require_once 'app/config.php'; // Tạo kết nối trong config.php
+require_once '/CosmeticWeb/config.php'; // Tạo kết nối trong config.php
 
 // Nếu kết nối thành công, chuyển hướng người dùng đến trang homepage
 header("Location: app/view/homepage.php");
