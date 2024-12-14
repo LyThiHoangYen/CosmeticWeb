b0a0efc6a9496ba5879de321b05c42643210c154
LyThiHoangYen <lythihoangyen20052003@gmail.com>
2024-12-14 16:19:11 +0700

    committtt

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
+<!-- 
+//ử dụng hàm header để thực hiện chuyển hướng
+//header("Location: app/view/homepage.php");
+//exit; 
+
+
+//require_once 'app/config.php'; -->
 <?php
-// Sử dụng hàm header để thực hiện chuyển hướng
+// Kết nối đến cơ sở dữ liệu
+require_once 'app/config.php'; // Tạo kết nối trong config.php
+
+// Nếu kết nối thành công, chuyển hướng người dùng đến trang homepage
 header("Location: app/view/homepage.php");
 exit; 
 ?>
