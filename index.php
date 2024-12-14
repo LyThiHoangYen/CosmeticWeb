517b1b9624fc37cb9e7ddf68de3c7a33ab0517e1
LyThiHoangYen <lythihoangyen20052003@gmail.com>
2024-12-15 03:48:29 +0700

    hihi

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-
 <?php
-// Sử dụng hàm header để thực hiện chuyển hướng
+// Đảm bảo không có bất kỳ dữ liệu nào xuất ra trước khi gọi header()
+
+// Chuyển hướng người dùng đến trang homepage
 header("Location: app/view/homepage.php");
-exit; 
+exit;
 ?>
